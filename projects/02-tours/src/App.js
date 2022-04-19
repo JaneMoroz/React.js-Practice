b4a02aae22457fa50679d32828fdb9a761104c66
Jane Moroz <Moroz.jane@yahoo.com>
2022-04-19 17:29:31 +0300

    Tours Project

--- projects/02-tours/src/App.js
+++ projects/02-tours/src/App.js
@@ -1,11 +1,68 @@
-import React, { useState, useEffect } from 'react'
-import Loading from './Loading'
-import Tours from './Tours'
+import React, { useState, useEffect } from "react";
+import Loading from "./Loading";
+import Tours from "./Tours";
 // ATTENTION!!!!!!!!!!
 // I SWITCHED TO PERMANENT DOMAIN
-const url = 'https://course-api.com/react-tours-project'
+const url = "https://course-api.com/react-tours-project";
 function App() {
-  return <h2>Tours Project Setup</h2>
+  // State Values
+  const [loading, setLoading] = useState(true);
+  const [tours, setTours] = useState([]);
+
+  // Remove tour
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id);
+    setTours(newTours);
+  };
+
+  // Fetch tours
+  const fetchTours = async () => {
+    setLoading(true);
+    try {
+      const res = await fetch(url);
+      const tours = await res.json();
+      setLoading(false);
+      setTours(tours);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  // Fetch tours only on initial render
+  useEffect(() => {
+    fetchTours();
+  }, []);
+
+  // If loading
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    );
+  }
+
+  // If all tours are deleted
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>no tours left</h2>
+          <button className="btn" onClick={() => fetchTours()}>
+            refresh
+          </button>
+        </div>
+      </main>
+    );
+  }
+
+  // If there is at least one tour
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  );
 }
 
-export default App
+export default App;
