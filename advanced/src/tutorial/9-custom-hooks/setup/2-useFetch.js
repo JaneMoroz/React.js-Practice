bbeae6f887263656ec8342530b756b79d157f539
JaneMoroz <moroz.jane@yahoo.com>
2022-04-24 18:30:41 +0300

    React.memo/ useCallback/useMemo

--- advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
+++ advanced/src/tutorial/9-custom-hooks/setup/2-useFetch.js
@@ -1,19 +1,22 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useFetch = (url) => {
   const [loading, setLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
+  // Just example
+  // useCallback deals with infinite loop
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setProducts(products);
     setLoading(false);
-  };
+  }, [url]);
 
+  // infinite loop because of getProducts
   useEffect(() => {
     getProducts();
-  }, [url]);
+  }, [url, getProducts]);
 
   return { loading, products };
 };
