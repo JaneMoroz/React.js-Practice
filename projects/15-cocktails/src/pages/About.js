b4cca95cb099cbf6c3fda026ed12feebe764cdca
JaneMoroz <moroz.jane@yahoo.com>
2022-05-02 19:22:09 +0300

    Cocktails Project Final

--- projects/15-cocktails/src/pages/About.js
+++ projects/15-cocktails/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Delectus odio
+        quaerat animi dolorum, aperiam fuga tempora eos soluta tenetur debitis
+        illum, quisquam officia earum illo doloribus, optio odit iure ipsum
+        tempore? Aperiam aliquam tempore saepe assumenda consequuntur culpa
+        aspernatur voluptatibus?
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
