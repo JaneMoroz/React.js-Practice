8af2206b11d1e90a4738634bda4519fdb620dfac
JaneMoroz <moroz.jane@yahoo.com>
2022-05-08 16:50:02 +0300

    Added Charts

--- github-search-users/src/components/Charts/Column3D.js
+++ github-search-users/src/components/Charts/Column3D.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.gammel";
 
-const Column3D = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "column3d",
+    width: "100%",
+    height: 400,
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Most Popular",
+        theme: "gammel",
+        yAxisName: "Stars",
+        xAxisName: "Repos",
+        xAxisNameFontSize: "16px",
+        yAxisNameFontSize: "16px",
+      },
+      data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default Column3D;
+export default ChartComponent;
--- github-search-users/src/components/Charts/ExampleChart.js
+++ github-search-users/src/components/Charts/ExampleChart.js
@@ -1,7 +1,30 @@
-import React from 'react';
+import React from "react";
+import FusionCharts from "fusioncharts";
+import Charts from "fusioncharts/fusioncharts.charts";
+import ReactFC from "react-fusioncharts";
+import FusionTheme from "fusioncharts/themes/fusioncharts.theme.gammel";
 
-const ExampleChart = () => {
-  return <div>chart</div>;
+ReactFC.fcRoot(FusionCharts, Charts, FusionTheme);
+
+const ChartComponent = ({ data }) => {
+  const chartConfigs = {
+    type: "column2d",
+    width: 400,
+    height: 400,
+    dataFormat: "json",
+    dataSource: {
+      chart: {
+        caption: "Countries With Most Oil Reserves [2017-18]",
+        subCaption: "In MMbbl = One Million barrels",
+        xAxisName: "Country",
+        yAxisName: "Reserves (MMbbl)",
+        numberSuffix: "%",
+        theme: "gammel",
+      },
+      data: data,
+    },
+  };
+  return <ReactFC {...chartConfigs} />;
 };
 
-export default ExampleChart;
+export default ChartComponent;
