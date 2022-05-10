392ee4906e61cdfccced6ca7eb1a43d636b6be65
JaneMoroz <moroz.jane@yahoo.com>
2022-05-10 15:01:48 +0300

    Added Auth0 Authentication

--- github-search-users/src/App.js
+++ github-search-users/src/App.js
@@ -4,14 +4,23 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        // TODO: wrap dashboard in private route later
-        <Route path="/" element={<Dashboard />} />
-        <Route path="login" element={<Login />} />
-        <Route path="*" element={<Error />} />
-      </Routes>
-    </Router>
+    <AuthWrapper>
+      <Router>
+        <Routes>
+          // TODO: wrap dashboard in private route later
+          <Route
+            path="/"
+            element={
+              <PrivateRoute>
+                <Dashboard />
+              </PrivateRoute>
+            }
+          />
+          <Route path="login" element={<Login />} />
+          <Route path="*" element={<Error />} />
+        </Routes>
+      </Router>
+    </AuthWrapper>
   );
 }
 
